8e937f7c7459a201dccae17c55940e49b93d0957
Tomasz Gasienica-Szostak <casprovy.tgs@gmail.com>
2019-07-05 15:39:11 +0200

    working on Polaris

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,8 @@ import { TextStyle } from '@shopify/polaris';
 
 const Index = () => (
 	<div>
-		<TextStyle variation='positive'>Hello Next.js TEST</TextStyle>
+		<TextStyle variation='positive'>Hello Next.js TEST RTRealm</TextStyle>
 	</div>
 );
 
 export default Index;
-
